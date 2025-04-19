87423e9455045ddfa11e5ba9273d6894438ffc6f
shelkea <Avinash.Shelke@hmhco.com>
2025-04-19 16:53:22 +0530

    first commit

---
+++ src/main/java/com/practice/modular/modulith/ModularModulithApplication.java
@@ -0,0 +1,13 @@
+package com.practice.modular.modulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModularModulithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModularModulithApplication.class, args);
+	}
+
+}
